4d33e81f382e36ec859117f1a08b335ddfd8dbb6
Felipe Fierro <felipefv@MacBook-Air-de-Felipe.local>
2017-08-24 15:15:38 -0600

    Lol

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
 
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  //propiedades
+  user:Observable<firebase.User>;
+  items: FirebaseListObservable<any[]>;
+  msgVal: string = '';
+  productos:FirebaseListObservable<any[]>;
+  producto:any = {};
+
+  constructor(public afAuth: AngularFireAuth, public af: AngularFireDatabase){
+    this.items = af.list('/messages',{
+      query:{
+        limitToLast:50
+      }
+    });
+    this.productos = af.list('/productos');
+    this.user = this.afAuth.authState;
+  }
+
+  login(){
+    this.afAuth.auth.signInAnonymously();
+  }
+
+  logout(){
+    this.afAuth.auth.signOut();
+  }
+
+  Send(desc: string){
+    this.items.push({message: desc});
+    this.msgVal='';
+  }
+
+  borrar(){
+    this.producto.Estado = true;
+    this.producto.FechaCreacion = new Date().getTime();
+    this.producto.RutaImagen = "";
+    this.productos.push(this.producto);
+    this.producto = {};
+    console.log(this.producto);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule} from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
+
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth'
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAUeHaMLL5NGJ6T9EiJe6ouCE4IBMJiLN4",
+  authDomain: "paginaweb-b7a9c.firebaseapp.com",
+  databaseURL: "https://paginaweb-b7a9c.firebaseio.com",
+  projectId: "paginaweb-b7a9c",
+  storageBucket: "paginaweb-b7a9c.appspot.com",
+  messagingSenderId: "142362252653"
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
